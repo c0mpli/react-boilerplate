76dce60f2c2b02d92bcc7f064391c328ae01c773
c0mpli <shirothegamer69@gmail.com>
2024-04-15 21:07:52 +0530

    counter added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 function App() {
+  const [counter, setCounter] = React.useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +10,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+          <button onClick={() => setCounter((p) => p - 1)}>-</button>
+          <p>{counter}</p>
+          <button onClick={() => setCounter((p) => p + 1)}>+</button>
+        </div>
       </header>
     </div>
   );
